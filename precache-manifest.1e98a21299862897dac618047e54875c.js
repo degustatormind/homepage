171c472de44533a54321c392611cff75d7520a95
degustatormind <degustatormind@gmail.com>
2019-10-29 04:40:00 +0600

    Updates

---
+++ precache-manifest.1e98a21299862897dac618047e54875c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "907588a2b62227961c94dc4ebe60af87",
+    "url": "/homepage/index.html"
+  },
+  {
+    "revision": "b24972f70e04a10ac4c5",
+    "url": "/homepage/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "947abe3e4a62357c714a",
+    "url": "/homepage/static/js/2.017bb613.chunk.js"
+  },
+  {
+    "revision": "b24972f70e04a10ac4c5",
+    "url": "/homepage/static/js/main.ad47395c.chunk.js"
+  },
+  {
+    "revision": "8d86f860bc75cbae7e34",
+    "url": "/homepage/static/js/runtime-main.b42b9dbc.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/homepage/static/media/logo.25bf045c.svg"
+  }
+]);
